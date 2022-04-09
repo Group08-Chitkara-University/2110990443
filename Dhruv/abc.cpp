1a948538b148d7289ec71c96d6924b6af1c0fc51
Dhruv0443 <dhruv0443.be21@chitkara.edu.in>
2022-04-09 21:14:55 +0530

    Add

---
+++ Dhruv/abc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,a,b;
+	cin>>n;
+	for(a=1;a<=n;a++)
+	{
+		int b;
+		for(b=1;b<=a;b++)
+		{
+			cout<<a+b;
+			b=b+1;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
